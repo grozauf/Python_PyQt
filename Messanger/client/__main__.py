f5a8402db389ba76db68808b5245ceb1e43abf08
Pavel Lobanov <lobanov.ps@gmail.com>
2019-05-22 21:20:52 +0300

    Домашнее задание для второго урока

--- Messanger/client/__main__.py
+++ Messanger/client/__main__.py
@@ -4,21 +4,81 @@ import datetime
 import logging
 import jim
 import settings
+import dis
+
+class ClientVerifier(type):
+
+    def __init__(self, clsname, bases, clsdict):
+        self.verify_class(clsname, clsdict)
+        type.__init__(self, clsname, bases, clsdict)
+
+    def verify_class(self, clsname, clsdict):
+
+        socket_argval = None
+        for key, value in clsdict.items():
+            assert not isinstance(value, socket.socket), 'Creating socket in class level is forbidden'
+
+            instructions_list = dis.get_instructions(value)
+            for instruction in instructions_list:
+                if instruction.argval == 'socket' and instruction.opname == 'LOAD_GLOBAL':
+                    while instruction.opname != 'STORE_ATTR':
+                        instruction = next(instructions_list)
+                        if instruction.opname == 'LOAD_ATTR' and instruction.arg == 2:
+                            assert instruction.argval == 'SOCK_STREAM', 'Only SOCK_STREAM sockets is available'
+                    socket_argval = instruction.argval
+
+        if socket_argval:
+            for key, value in clsdict.items():
+                forbidden_procs = ['listen', 'accept']
+                instructions_list = dis.get_instructions(value)
+                for instruction in instructions_list:
+                    if instruction.argval == socket_argval:
+                        next_instruction = next(instructions_list)
+                        assert not (next_instruction.argval in forbidden_procs and
+                                    next_instruction.opname == 'LOAD_ATTR'), \
+                            f"{clsname} have forbidden method {next_instruction.argval}"
 
 
-def get_presence_msg(action, data):
-    time = datetime.datetime.now()
-    msg = {
-        "action": action,
-        "time": time.isoformat(),
-        "user": {
-                "account_name":  "anonim",
-                "status":      "Yep, I am here!"
-        },
-        "data": data
-    }
+class Client(metaclass=ClientVerifier):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = None
 
-    return jim.pack(msg)
+    def make_connection(self):
+        self.sock = socket.socket()
+        self.sock.connect((self.host, self.port))
+        logger.info(f'Client started with {self.host}:{self.port}')
+        print(f'Client started with {self.host}:{self.port}')
+
+    def get_presence_msg(self, action, data):
+        time = datetime.datetime.now()
+        msg = {
+            "action": action,
+            "time": time.isoformat(),
+            "user": {
+                    "account_name":  "anonim",
+                    "status":      "Yep, I am here!"
+            },
+            "data": data
+        }
+
+        return jim.pack(msg)
+
+    def send_request(self):
+        while True:
+            action = input('Enter action to send:')
+            data = input('Enter data to send:')
+
+            msg = self.get_presence_msg(action, data)
+            self.sock.sendall(msg)
+
+    def recv_response(self):
+        while True:
+            response = self.sock.recv(settings.BUFFERSIZE)
+            response = jim.unpack(response)
+            logger.info(f'Got next response from server: {response}')
+            print(f'Got next response from server: {response}')
 
 
 host = getattr(settings, 'HOST', '127.0.0.1')
@@ -47,24 +107,14 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 try:
-    sock = socket.socket()
-    sock.connect((host, port))
-    logger.info(f'Client started with {host}:{port}')
-    print(f'Client started with {host}:{port}')
+
+    clientObj = Client(host, port)
+    clientObj.make_connection()
 
     if args.mode == 'w':
-        while True:
-            action = input('Enter action to send:')
-            data = input('Enter data to send:')
-
-            msg = get_presence_msg(action, data)
-            sock.sendall(msg)
+        clientObj.send_request()
     else:
-        while True:
-            response = sock.recv(settings.BUFFERSIZE)
-            response = jim.unpack(response)
-            logger.info(f'Got next response from server: {response}')
-            print(f'Got next response from server: {response}')
+        clientObj.recv_response()
 
 except KeyboardInterrupt:
     logger.info('Client  closed')
--- Messanger/server/__main__.py
+++ Messanger/server/__main__.py
@@ -6,6 +6,7 @@ import select
 import threading
 import jim
 import settings
+import dis
 from routes import resolve, get_server_routes
 from protocol import (
     validate_request, make_response,
@@ -14,22 +15,111 @@ from protocol import (
 
 from handlers import handle_request
 
+class ServerVerifier(type):
 
-def get_response_msg():
-    msg = {
-        "response": 200,
-        "alert": "Необязательное сообщение/уведомление"
-    }
-    return jim.pack(msg)
+    def __init__(self, clsname, bases, clsdict):
+        self.verify_class(clsname, clsdict)
+        type.__init__(self, clsname, bases, clsdict)
+
+    def verify_class(self, clsname, clsdict):
+
+        socket_argval = None
+        for key, value in clsdict.items():
+            assert not isinstance(value, socket.socket), 'Creating socket in class level is forbidden'
+
+            instructions_list = dis.get_instructions(value)
+            for instruction in instructions_list:
+                if instruction.argval == 'socket' and instruction.opname == 'LOAD_GLOBAL':
+                    while instruction.opname != 'STORE_ATTR':
+                        instruction = next(instructions_list)
+                        if instruction.opname == 'LOAD_ATTR' and instruction.arg == 2:
+                            assert instruction.argval == 'SOCK_STREAM', 'Only SOCK_STREAM sockets is available'
+                    socket_argval = instruction.argval
+
+        if socket_argval:
+            for key, value in clsdict.items():
+                forbidden_procs = ['connect']
+                instructions_list = dis.get_instructions(value)
+                for instruction in instructions_list:
+                    if instruction.argval == socket_argval:
+                        next_instruction = next(instructions_list)
+                        assert not (next_instruction.argval in forbidden_procs and
+                                    next_instruction.opname == 'LOAD_ATTR'), \
+                            f"{clsname} has forbidden method {next_instruction.argval}"
 
 
-def read_client_data(client, requests, buffersize):
-    b_request = client.recv(buffersize)
-    requests.append(b_request)
+class PortDescriptor:
+
+    def __init__(self):
+        self._value = 7777
+
+    def __get__(self, instance, instance_type):
+        return self._value
+
+    def __set__(self, instance, value):
+        if type(value) is not int:
+            raise TypeError('Value must be integer')
+        if not value >= 0:
+            raise ValueError('Port number must be => 0')
+        self._value = value
 
 
-def write_client_data(client, response):
-    client.send(response)
+class Server(metaclass=ServerVerifier):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = PortDescriptor()
+        self.port = port
+        self.sock = None
+
+    def make_listen(self):
+        self.sock = socket.socket()
+        self.sock.bind((self.host, self.port))
+        self.sock.settimeout(0)
+        self.sock.listen(5)
+        logging.info(f'Server started with {self.host}:{self.port}')
+
+    def read_client_data(self, client, requests, buffersize):
+        b_request = client.recv(buffersize)
+        requests.append(b_request)
+
+    def write_client_data(self, client, response):
+        client.send(response)
+
+    def run_event_loop(self):
+        requests = []
+        connections = []
+
+        while True:
+            try:
+                client, address = self.sock.accept()
+                logging.info(f'Client detected {address}')
+                connections.append(client)
+            except Exception:
+                pass
+
+            if connections != []:
+                rlist, wlist, xlist = select.select(
+                    connections, connections, connections, 0
+                )
+
+                for r_client in rlist:
+                    thread = threading.Thread(
+                        target=self.read_client_data,
+                        args=(r_client, requests, settings.BUFFERSIZE)
+                    )
+                    thread.start()
+
+                if requests:
+                    b_request = requests.pop()
+                    b_response = handle_request(b_request)
+
+                    for w_client in wlist:
+                        thread = threading.Thread(
+                            target=self.write_client_data,
+                            args=(w_client, b_response)
+                        )
+                        thread.start()
+
 
 
 host = getattr(settings, 'HOST', '127.0.0.1')
@@ -62,46 +152,10 @@ logging.basicConfig(
 
 def main():
 
-    requests = []
-    connections = []
-
+    serverObj = Server(host, port)
     try:
-        sock = socket.socket()
-        sock.bind((host, port))
-        sock.settimeout(0)
-        sock.listen(5)
-        logging.info(f'Server started with {host}:{port}')
-        while True:
-            try:
-                client, address = sock.accept()
-                logging.info(f'Client detected {address}')
-                connections.append(client)
-            except Exception:
-                pass
-
-            if connections != []:
-                rlist, wlist, xlist = select.select(
-                    connections, connections, connections, 0
-                )
-
-                for r_client in rlist:
-                    thread = threading.Thread(
-                        target=read_client_data,
-                        args=(r_client, requests, settings.BUFFERSIZE)
-                    )
-                    thread.start()
-
-                if requests:
-                    b_request = requests.pop()
-                    b_response = handle_request(b_request)
-
-                    for w_client in wlist:
-                        thread = threading.Thread(
-                            target=write_client_data,
-                            args=(w_client, b_response)
-                        )
-                        thread.start()
-
+        serverObj.make_listen()
+        serverObj.run_event_loop()
     except KeyboardInterrupt:
         logging.info('Server  closed')
 
